84044de65fc283b7e067ced6ab4539b762b55ca3
Karen Kim <kyoun5546@gmail.com>
2020-12-09 01:29:36 +0900

    GameManager

---
+++ Jelly_Punch/Assets/Script/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int totalPoint;
+    public int stagePoint;
+    public int stageIndex;
+    public int health;
+    public PlayerMove player;
+    public void NextStage()
+    {
+        stageIndex++;
+
+        totalPoint += stagePoint;
+        stagePoint = 0;
+    }
+    
+    public void HealthDown()
+    {
+        if (health > 0)
+            health--;
+        else {
+            //Player Die Effect
+            player.OnDie();
+
+            //Result UI
+            Debug.Log("Game Over!");
+            // Restart button UI            
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+            HealthDown();
+
+            //Falling speed == 0
+            collision.attachedRigidbody.velocity = Vector2.zero;
+    }
+}
